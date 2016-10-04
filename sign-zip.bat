60a206872612ad54dbb2e9ad3887ecb81d20d529
afonte <mandy271088@yahoo.es>
2016-10-04 16:13:04 -0500

    use relative paths for the files

--- sign-zip.bat
+++ sign-zip.bat
@@ -1,2 +1,5 @@
-keytool -genkeypair -alias mykey -keyalg RSA -keystore key.keystore -validity 180 
-jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore key.keystore Update.zip mykey
+set KEYSTORE_FILE=%~dp0key.keystore
+set FILE_TO_SIGN=%~dp0Update.zip
+keytool -genkeypair -alias mykey -keyalg RSA -keystore %KEYSTORE_FILE% -validity 180 
+jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore %KEYSTORE_FILE% %FILE_TO_SIGN% mykey
+Pause
